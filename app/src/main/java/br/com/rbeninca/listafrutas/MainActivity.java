2fd31829a2dd91b37d868d6bb5e52220fc3e8375
rbeninca <rbeninca@gmail.com>
2019-11-11 17:07:09 -0300

    Implementação do codigo de view para chamada do listView

--- app/src/main/java/br/com/rbeninca/listafrutas/MainActivity.java
+++ app/src/main/java/br/com/rbeninca/listafrutas/MainActivity.java
@@ -3,12 +3,26 @@ package br.com.rbeninca.listafrutas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listView);
+
+        FrutaController frutaController = new FrutaController();
+
+        FrutaAdapter  frutaAdapter =new FrutaAdapter(
+                getApplicationContext(),
+                R.layout.template_item_fruta,
+                frutaController.FRUTAS
+                );
+
     }
 }

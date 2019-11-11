702dcb9a560149455b3d1290c0633b1415faeb06
rbeninca <rbeninca@gmail.com>
2019-11-11 16:58:24 -0300

    Implementação layout item  listagem

--- app/src/main/java/br/com/rbeninca/listafrutas/FrutaAdapter.java
+++ app/src/main/java/br/com/rbeninca/listafrutas/FrutaAdapter.java
@@ -1,12 +1,18 @@
 package br.com.rbeninca.listafrutas;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.util.zip.Inflater;
 
 public class FrutaAdapter  extends ArrayAdapter<Fruta> {
 
@@ -21,8 +27,18 @@ public class FrutaAdapter  extends ArrayAdapter<Fruta> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
 
+        //Criando  propriedade LayoutInflater
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        convertView = layoutInflater.inflate(mResource,parent,false);
 
+        //Associando elementos da view inflada a váriaveis locais
+        TextView tvCodigo = (TextView) convertView.findViewById(R.id.codigo);
+        TextView tvNome = (TextView) convertView.findViewById(R.id.nome);
+        TextView tvPreco =  (TextView) convertView.findViewById(R.id.preco);
+        TextView tvPrecoVenda =(TextView) convertView.findViewById(R.id.preco_venda);
+        ImageView imgView = (ImageView) convertView.findViewById(R.id.imageView)
+
+        return convertView;
     }
 }

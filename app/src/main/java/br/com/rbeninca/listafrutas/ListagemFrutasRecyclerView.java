5cc1ed6d5cc316627d382bb221669d0e38fade87
rbeninca <rbeninca@gmail.com>
2019-11-14 01:09:43 -0300

    Implementação do listener ClickItem para o RecyclerView

--- app/src/main/java/br/com/rbeninca/listafrutas/ListagemFrutasRecyclerView.java
+++ app/src/main/java/br/com/rbeninca/listafrutas/ListagemFrutasRecyclerView.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class ListagemFrutasRecyclerView extends AppCompatActivity {
     RecyclerView recyclerView;
+    FrutaController frutaController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +26,7 @@ public class ListagemFrutasRecyclerView extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
 
-        FrutaController frutaController =new FrutaController();
+         frutaController =new FrutaController();
 
         FrutaAdapterRecyclerView frutaAdapterRecyclerView=new FrutaAdapterRecyclerView(
                 getApplicationContext(),
@@ -33,6 +37,30 @@ public class ListagemFrutasRecyclerView extends AppCompatActivity {
         recyclerView.setAdapter(frutaAdapterRecyclerView);
 
 
+        recyclerView.addOnItemTouchListener( recyclerItemClickListener);
+
 
     }
+
+    public RecyclerItemClickListener recyclerItemClickListener= new RecyclerItemClickListener(
+            getBaseContext(),
+            recyclerView,
+            new RecyclerItemClickListener.OnItemClickListener() {
+                @Override
+                public void onItemClick(View view, int position) {
+                    Intent i = new Intent (getApplicationContext(),ExibeFrutaActivity.class);
+                    i.putExtra("id", position );
+                    startActivity(i);
+                }
+
+                @Override
+                public void onLongItemClick(View view, int position) {
+
+                }
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                }
+            });
 }
